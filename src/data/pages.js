a5cd502c52d4e107c5ea3cbcea694a678cd345f4
Pedja Stojiljkovic <pedja1310@gmail.com>
2023-02-07 15:34:50 +0100

    added more pages data for prototyping

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,4 +1,4 @@
-export const pages = [
+const pages = [
   {
     title: "Title 1",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
@@ -7,15 +7,6 @@ export const pages = [
     category: "news",
     active: true,
     id: "0",
-  },
-  {
-    title: "Title 2",
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
-    author: "Author 4",
-    date: new Date(2023, 1, 1).toDateString(),
-    category: "insights",
-    active: false,
-    id: "1",
   },
   {
     title: "Title 3",
@@ -30,7 +21,7 @@ export const pages = [
     title: "Title 4",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
     author: "Author 1",
-    date: new Date(2023, 0, 21).toDateString(),
+    date: new Date(2023, 0, 8).toDateString(),
     category: "news",
     active: false,
     id: "3",
@@ -46,7 +37,7 @@ export const pages = [
     id: "4",
   },
   {
-    title: "Title 5",
+    title: "Title 2",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
     author: "Author 3",
     date: "18.02.2023.",
@@ -55,4 +46,62 @@ export const pages = [
     active: true,
     id: "5",
   },
+  {
+    title: "Title 8",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 1",
+    date: new Date(2023, 1, 3).toDateString(),
+    category: "reports",
+    active: true,
+    id: "6",
+  },
+  {
+    title: "Title 11",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 3",
+    date: new Date(2023, 1, 1).toDateString(),
+    category: "news",
+    active: false,
+    id: "7",
+  },
+  {
+    title: "Title 6",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 1",
+    date: new Date(2023, 0, 13).toDateString(),
+    category: "insights",
+    active: true,
+    id: "8",
+  },
+  {
+    title: "Title 14",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 4",
+    date: new Date(2023, 1, 2).toDateString(),
+    category: "news",
+    active: true,
+    id: "9",
+  },
+  {
+    title: "Title 13",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 2",
+    date: "02.01.2023.",
+    date: new Date(2023, 0, 2).toDateString(),
+    category: "insights",
+    active: false,
+    id: "10",
+  },
+  {
+    title: "Title 17",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque facere accusantium, perferendis aspernatur ipsa animi totam sed, esse, quam nisi possimus explicabo deleniti placeat temporibus corporis blanditiis quas delectus amet.",
+    author: "Author 4",
+    date: "18.02.2023.",
+    date: new Date(2023, 1, 22).toDateString(),
+    category: "reports",
+    active: false,
+    id: "11",
+  },
 ];
+
+export default pages;
