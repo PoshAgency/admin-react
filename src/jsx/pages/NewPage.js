3c2a4b099294ddcd97514f783fb6a9f51b3831ac
Pedja Stojiljkovic <pedja1310@gmail.com>
2023-02-22 16:04:35 +0100

    add ckeditor and new page layout basics finished

--- src/jsx/pages/NewPage.js
+++ src/jsx/pages/NewPage.js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 import PageImagesUploader from "../components/PageImagesUploader";
 import slugify from "slugify";
 import BasicDatePicker from "../components/Forms/Pickers/MetarialDate";
+import { Editor } from "ckeditor5-custom-build/build/ckeditor";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+import "./ckeditor5.css";
 
 const NewPage = () => {
   const [pagePath, setPagePath] = useState("");
@@ -17,58 +21,130 @@ const NewPage = () => {
         </Link>
       </div>
       <div className="w-100 mt-5 card">
-        <form
-          onSubmit={(e) => e.preventDefault()}
-          className="d-flex justify-content-between card-body px-0"
-        >
-          <div className="col-8 w-100">
-            <h4>Page Title</h4>
-            <div className="form-group mt-3">
-              <input
-                type="text"
-                className="form-control input-default px-2 mb-3"
-                placeholder="Enter page title"
-                onChange={(e) => setPagePath(e.target.value)}
-              />
-              <span className="ml-3">{`https://theposh.agency/${slugify(
-                pagePath
-              )}`}</span>
+        <form onSubmit={(e) => e.preventDefault()} className="card-body">
+          <div className="row d-flex justify-content-between">
+            <div className="col-8 w-100">
+              <h3>Page Title</h3>
+              <div className="form-group mt-3">
+                <input
+                  type="text"
+                  className="form-control input-default px-2 mb-3"
+                  placeholder="Enter page title"
+                  onChange={(e) => setPagePath(e.target.value)}
+                />
+                <span className="ml-3">{`https://theposh.agency/${slugify(
+                  pagePath
+                )}`}</span>
+              </div>
+              <h3 className="mt-4">Hero Title</h3>
+              <div className="form-group mt-3">
+                <input
+                  type="text"
+                  className="form-control input-default px-2 mb-3"
+                  placeholder="Enter hero title"
+                />
+              </div>
+              <PageImagesUploader />
+              <h3 className="mt-4">Description</h3>
+              <div className="form-group">
+                <textarea
+                  className="form-control mt-3"
+                  rows="4"
+                  placeholder="Enter description"
+                  id="description"
+                ></textarea>
+              </div>
             </div>
-            <h4 className="mt-4">Hero Title</h4>
-            <div className="form-group mt-3">
-              <input
-                type="text"
-                className="form-control input-default px-2 mb-3"
-                placeholder="Enter hero title"
-              />
-            </div>
-            <PageImagesUploader />
-            <h4 className="mt-4">Description</h4>
-            <div className="form-group">
-              <textarea
-                className="form-control mt-3"
-                rows="4"
-                placeholder="Enter description"
-                id="description"
-              ></textarea>
+            <div className="col-4 w-100">
+              <div>
+                <h3 className="mb-3">Date Published</h3>
+                <BasicDatePicker style={{ paddingLeft: "12px" }} />
+              </div>
+              <div>
+                <h3 className="mb-3 mt-3">Template</h3>
+                <div className="form-group">
+                  <select className="form-control">
+                    <option value="">Select a template</option>
+                    <option value="homepage">Homepage</option>
+                    <option value="about-us">About Us</option>
+                    <option value="careers">Carrers</option>
+                    <option value="default-page">Default Page</option>
+                    <option value="team">Team</option>
+                  </select>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-4 w-100">
-            <div>
-              <h4 className="mb-3">Date Published</h4>
-              <BasicDatePicker style={{ paddingLeft: "12px" }} />
+          <div className="row d-flex flex-column mt-5">
+            <div className="col">
+              <h3 className="mb-3">
+                Page content{" "}
+                <span className="small ml-2">(Up to 240 characters)</span>
+              </h3>
+              <CKEditor
+                editor={Editor}
+                data="Enter page content"
+                onReady={(editor) => {
+                  // You can store the "editor" and use when it is needed.
+                  // editor.editing.view.change((writer) => {
+                  //   writer.setStyle(
+                  //     "height",
+                  //     "400px",
+                  //     editor.editing.view.document.getRoot()
+                  //   );
+                  //   console.log(editor.editing.view.document.getRoot());
+                  // });
+                }}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  console.log({ event, editor, data });
+                }}
+                onBlur={(event, editor) => {
+                  // console.log("Blur.", editor);
+                }}
+                onFocus={(event, editor) => {
+                  // console.log("Focus.", editor);
+                }}
+              />
             </div>
-            <div>
-              <h4 className="mb-3 mt-3">Template</h4>
-              <div className="form-group">
-                <select className="form-control">
-                  <option value="">Select a template</option>
-                  <option value="homepage">Homepage</option>
-                  <option value="about-us">About Us</option>
-                  <option value="careers">Carrers</option>
-                  <option value="default-page">Default Page</option>
-                  <option value="team">Team</option>
-                </select>
+          </div>
+          <div className="row mt-5">
+            <div className="col">
+              <div className="d-flex justify-content-between">
+                <h3>Page sections</h3>
+                <div>
+                  <Button variant="light" className="btn-sm mr-3">
+                    Add sections
+                  </Button>
+                  <Button variant="light" className="btn-sm">
+                    Collapse sections
+                  </Button>
+                </div>
+              </div>
+
+              <p>TODO: Programatically add new sections</p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col">
+              <h3>SEO</h3>
+              <div className="form-group mt-3 w-50 px-3">
+                <label htmlFor="seo-title">Title</label>
+                <input
+                  id="seo-title"
+                  type="text"
+                  className="form-control input-default px-3 mb-3"
+                  placeholder="SEO Title"
+                />
+              </div>
+              <div className="form-group mt-3 w-75 px-3">
+                <label htmlFor="seo-description">Description</label>
+                <textarea
+                  id="seo-description"
+                  rows={4}
+                  className="form-control input-default px-3"
+                  placeholder="Max 160 characters"
+                />
               </div>
             </div>
           </div>
