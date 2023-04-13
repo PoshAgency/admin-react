40d2c676859746fe6e1e1b2b56208c03c49febc8
Pedja Stojiljkovic <pedja1310@gmail.com>
2023-04-13 11:38:23 +0200

    finished order details

--- src/jsx/components/OrderTimeline.js
+++ src/jsx/components/OrderTimeline.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const OrderTimeline = () => {
-  return <div>OrderTimeline</div>;
+  const { selectedOrder } = useSelector((state) => state.orders);
+
+  return (
+    <div className="px-4">
+      <h4>Order timeline</h4>
+      <hr />
+      <p className="mb-1">
+        <span className="w-25 d-inline-block">Created:</span>
+        <b>{selectedOrder?.createdAt}</b>
+      </p>
+    </div>
+  );
 };
 
 export default OrderTimeline;
