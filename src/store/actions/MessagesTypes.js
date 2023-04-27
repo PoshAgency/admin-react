c1ea4dfda90f31ea6d9f9798f4f1bc31099efe09
Pedja Stojiljkovic <pedja1310@gmail.com>
2023-04-27 16:13:01 +0200

    added actions and types for message edit functionality

--- src/store/actions/MessagesTypes.js
+++ src/store/actions/MessagesTypes.js
@@ -2,3 +2,6 @@ export const TOGGLE_CHECK_MESSAGE = "[Messages Action] Toggle Check Message";
 export const DESELECT_ALL_MESSAGES = "[Message Action] Deselect All Messages";
 export const TOGGLE_SELECT_ALL_MESSAGES =
   "[Messages Action] Toggle Select All Messages";
+export const EDIT_MESSAGE = "[Messages Action] Edit Message";
+export const REMOVE_SELECTED_MESSAGE =
+  "[Messages Action] Remove Selected Message";
